7bc95c5e89798c7cd8220590738eeb600e271ba3
bmalkmus <bmalkmus23@gmail.com>
2020-11-09 23:57:25 -0800

    section 3 done

--- gallery/App.js
+++ gallery/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css'
+import "./style.css"
+
+const gallery = require("../DATA/gallery.json")
 
 function App(){
     return(
-        <div>
-            Test2
+        <div className="slide-container">
+            <Slide easing="ease">
+                {[...gallery].map(item =>{
+                    return(
+                        <div key = {item.src} className="each-slide">
+                            <div>
+                                <img src={item.src}/>
+                                <span>{item.description}</span>
+                            </div>
+                      </div>
+                    )
+                })}
+                
+            </Slide>
         </div>
     )
 }
-export default App
+export default App 
